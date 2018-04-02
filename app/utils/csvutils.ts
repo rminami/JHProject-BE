509f425db77e8f7ae5bcd5e054b35358cbcaaa90
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-02 17:25:26 +0100

    Switched to 2 spaces

--- app/utils/csvutils.ts
+++ app/utils/csvutils.ts
@@ -10,59 +10,59 @@ import * as csv from 'fast-csv'
 import * as firstline from 'firstline'
 
 interface ICsvMetadata {
-    columns: [{
-        header: string,
-        type: 'number' | 'category',
-    }],
-    rows: number
+  columns: [{
+    header: string,
+    type: 'number' | 'category',
+  }],
+  rows: number
 }
 
 /** Retrieves CSV headers for the initial load */
 export const getCsvHeaders = async (filepath: string): Promise<any> => {
-    const line = await firstline(filepath)
-    const headerArr = line.split(',').map(item => item.replace(/^"?(.+?)"?$/, '$1'))
+  const line = await firstline(filepath)
+  const headerArr = line.split(',').map(item => item.replace(/^"?(.+?)"?$/, '$1'))
 
-    return new Promise((resolve, reject) => {
-        const sets = headerArr.map(_ => new Set())
-        let rows = -1 // To account for the header line
+  return new Promise((resolve, reject) => {
+    const sets = headerArr.map(_ => new Set())
+    let rows = -1 // To account for the header line
 
-        fs.createReadStream(filepath).pipe(csv())
-            .on('data', data => {
-                sets.map((_, index) => sets[index].add(data[index]))
-                rows++
+    fs.createReadStream(filepath).pipe(csv())
+      .on('data', data => {
+        sets.map((_, index) => sets[index].add(data[index]))
+        rows++
 
-            }).on('end', () => {
-                const columns = sets.map((set, index) => {
-                    if (set.size <= 5) {
-                        return ({
-                            header: headerArr[index],
-                            type: 'category',
-                        })
-                    } else {
-                        return ({
-                            header: headerArr[index],
-                            type: 'number',
-                        })
-                    }
-                })
-                resolve({ columns, rows })
-            }).on('error', err => {
-                reject(err)
+      }).on('end', () => {
+        const columns = sets.map((set, index) => {
+          if (set.size <= 5) {
+            return ({
+              header: headerArr[index],
+              type: 'category',
             })
-    })
+          } else {
+            return ({
+              header: headerArr[index],
+              type: 'number',
+            })
+          }
+        })
+        resolve({ columns, rows })
+      }).on('error', err => {
+        reject(err)
+      })
+  })
 }
 
 /** Retrieves data from specific columns within a CSV */
 export const getCsvColumns = async (filepath: string, columns: number[]): Promise<any> => {
-    return new Promise((resolve, reject) => {
-        const result = []
-        fs.createReadStream(filepath).pipe(csv())
-            .on('data', data => {
-                result.push(columns.map(col => data[col]))
-            }).on('end', () => {
-                resolve(result)
-            }).on('error', err => {
-                reject(err)
-            })
-    })
+  return new Promise((resolve, reject) => {
+    const result = []
+    fs.createReadStream(filepath).pipe(csv())
+      .on('data', data => {
+        result.push(columns.map(col => data[col]))
+      }).on('end', () => {
+        resolve(result)
+      }).on('error', err => {
+        reject(err)
+      })
+  })
 }
--- app/utils/fsutils.ts
+++ app/utils/fsutils.ts
@@ -12,155 +12,155 @@ import { promisify } from 'util'
 import * as crypto from 'crypto'
 import { secret } from '../../config/secret'
 
+/**
+ * @interface
+ * Interface for an object representing the metadata of a file or directory.
+ */
 interface IFileEntry {
-    file_path: string,
-    file_name: string,
-    id: string,
-    supported_views: {},
-    type: 'directory' | 'tabular' | 'scalable_image' | 'file',
-    metadata: {},
-    status: 'uploading' | 'preprocessing' | 'ready',
-    children?: IFileEntry[]
+  file_path: string,
+  file_name: string,
+  id: string,
+  supported_views: {},
+  type: 'directory' | 'tabular' | 'scalable_image' | 'file',
+  metadata: {},
+  status: 'uploading' | 'preprocessing' | 'ready',
+  children?: IFileEntry[]
 }
 
 /**
  * Return a list of files contained in the specified directory.
  *
- * @param {string} parentPath - Path to the parent directory to look in.
+ * @param {string} dirPath - Path to the parent directory to look in.
  * @returns {Promise<IFileEntry[]>} A sorted list of file entries, complete with metadata.
  */
-export const listFiles = async (parentPath: string): Promise<IFileEntry[]> => {
-    const fullPath: string = path.join(__dirname, '../..', parentPath)
-    const fileNameArr: string[] = await promisify(fs.readdir)(fullPath)
+export const listFiles = async (dirPath: string): Promise<IFileEntry[]> => {
+  const fullPath: string = path.join(__dirname, '../..', dirPath)
+  const fileNameArr: string[] = await promisify(fs.readdir)(fullPath)
 
-    const filelist: IFileEntry[] = await scanFiles(parentPath, fileNameArr)
-    return filelist.filter(file => file !== undefined)
-        .sort(compareEntries)
+  const filelist: IFileEntry[] = await scanFiles(dirPath, fileNameArr)
+  return filelist.filter(file => file !== undefined)
+    .sort(compareEntries)
 }
 
 /**
- * Returns an array of file entries, each with the metadata
- *     as required in the protocol.
+ * Returns an array of file entry objects, each containing the metadata for a
+ *     file in the specified directory.
  *
- * @param {string} parentPath - Path to the parent directory to look in.
- * @param {string[]} fileNameArr - Array of names of files contained in directory.
- * @returns {Promise<IFileEntry[]>} Array of files complete with metadata.
+ * @param {string} parentPath - Path of the directory to scan.
+ * @param {string[]} fileNameArr - Array of all of the items in the specified directory.
+ * @returns {Promise<IFileEntry[]>} a Promise for all of the files' metadata.
  */
 const scanFiles = (parentPath: string, fileNameArr: string[]): Promise<IFileEntry[]> => {
-    return Promise.all(fileNameArr.map(async fileName => {
-        // Files whose names start with '.' should not be displayed.
-        if (fileName.startsWith('.')) {
-            return undefined
-        }
-        try {
-            const filePath: string = path.join(parentPath, fileName)
-            return await getFileEntry(filePath)
-        } catch (e) {
-            console.log('Could not get stats for file ' + fileName)
-            return undefined
-        }
-    }))
+  return Promise.all(fileNameArr.map(async fileName => {
+
+    // Files whose names start with '.' should not be displayed.
+    if (fileName.startsWith('.')) {
+      return undefined
+    }
+    try {
+      const filePath: string = path.join(parentPath, fileName)
+      return await getFileEntry(filePath)
+    } catch (e) {
+      throw new Error('Could not get stats for file ' + fileName)
+    }
+  }))
 }
 
 // Initial value for the metadata field as specified in the protocol.
-const initialMetadata = {
-    version: 1,
-    namespaces: {},
-}
+const initialMetadata = Object.freeze({
+  version: 1,
+  namespaces: {},
+})
 
 /**
- * Returns an entry for the file specified, complete with metadata.
+ * Returns an object containing the metadata for the selected file or directory.
  *
  * @param {string} filePath - Path to the file to retrieve metadata for.
- * @returns {Promise<IFileEntry>} File entry containing all of the relevant metadata.
+ * @returns {Promise<IFileEntry>} Object containing metadata for selected file.
  */
 export const getFileEntry = async (filePath: string): Promise<IFileEntry> => {
-    const file: string = path.basename(filePath)
-    const stats: fs.Stats = await promisify(fs.lstat)(path.join(__dirname, '../..', filePath))
+  const file: string = path.basename(filePath)
+  const stats: fs.Stats = await promisify(fs.lstat)(path.join(__dirname, '../..', filePath))
 
-    const cipher: crypto.Cipher = crypto.createCipher('aes192', secret)
-    const id: string = cipher.update(filePath, 'utf8', 'hex') + cipher.final('hex')
+  const cipher: crypto.Cipher = crypto.createCipher('aes192', secret)
+  const id: string = cipher.update(filePath, 'utf8', 'hex') + cipher.final('hex')
 
-    if (stats.isDirectory()) {
-        return {
-            file_path: filePath,
-            file_name: file,
-            id,
-            supported_views: {
-                meta: null,
-                raw: {
-                    size: stats.size,
-                },
-            },
-            type: 'directory',
-            metadata: initialMetadata,
-            status: 'ready',
-        }
+  if (stats.isDirectory()) {
+    return {
+      file_path: filePath,
+      file_name: file,
+      id,
+      supported_views: {
+        raw: {
+          size: stats.size,
+        },
+      },
+      type: 'directory',
+      metadata: initialMetadata,
+      status: 'ready',
     }
-    const ext = path.extname(file)
-    switch (ext) {
-        case '.csv':
-            return {
-                file_path: filePath,
-                file_name: file,
-                id,
-                supported_views: {
-                    meta: null,
-                    raw: {
-                        size: stats.size,
-                    },
-                    tabular: {
-                        // Although the spec specifies metadata to be placed here,
-                        // this is unnecessary for the file view.
-                    },
-                },
-                type: 'tabular',
-                metadata: initialMetadata,
-                status: 'ready',
-            }
+  }
+  const ext = path.extname(file)
+  switch (ext) {
+    case '.csv':
+      return {
+        file_path: filePath,
+        file_name: file,
+        id,
+        supported_views: {
+          raw: {
+            size: stats.size,
+          },
+          tabular: {
+            // Although the spec specifies metadata to be placed here,
+            // this is unnecessary for the file view.
+          },
+        },
+        type: 'tabular',
+        metadata: initialMetadata,
+        status: 'ready',
+      }
 
-        case '.png':
-        case '.jpg':
-        case '.dzi':
-            return {
-                file_path: filePath,
-                file_name: file,
-                id,
-                supported_views: {
-                    meta: null,
-                    raw: {
-                        size: stats.size,
-                    },
-                    scalable_image: {
-                        // Although the spec specifies metadata to be placed here,
-                        // this is unnecessary for the file view.
-                    },
-                },
-                type: 'scalable_image',
-                metadata: initialMetadata,
-                status: 'ready',
-            }
+    case '.png':
+    case '.jpg':
+    case '.dzi':
+      return {
+        file_path: filePath,
+        file_name: file,
+        id,
+        supported_views: {
+          raw: {
+            size: stats.size,
+          },
+          scalable_image: {
+            // Although the spec specifies metadata to be placed here,
+            // this is unnecessary for the file view.
+          },
+        },
+        type: 'scalable_image',
+        metadata: initialMetadata,
+        status: 'ready',
+      }
 
-        default:
-            return {
-                file_path: filePath,
-                file_name: file,
-                id,
-                supported_views: {
-                    meta: null,
-                    raw: {
-                        size: stats.size,
-                    },
-                },
-                type: 'file',
-                metadata: initialMetadata,
-                status: 'ready',
-            }
-    }
+    default:
+      return {
+        file_path: filePath,
+        file_name: file,
+        id,
+        supported_views: {
+          raw: {
+            size: stats.size,
+          },
+        },
+        type: 'file',
+        metadata: initialMetadata,
+        status: 'ready',
+      }
+  }
 }
 
 /**
- * Sorts entries so directories are at the top (like GitHub)
+ * Sorts entries in alphabetical order.
  *
  * @param {IFileEntry} entry1 The first file to compare.
  * @param {IFileEntry} entry2 The second file to compare.
@@ -168,11 +168,5 @@ export const getFileEntry = async (filePath: string): Promise<IFileEntry> => {
  *                    1 if the second entry should go before the first entry.
  */
 const compareEntries = (entry1: IFileEntry, entry2: IFileEntry): number => {
-    if (entry1.type === 'directory' && entry2.type !== 'directory') {
-        return -1
-    } else if (entry1.type !== 'directory' && entry2.type === 'directory') {
-        return 1
-    } else {
-        return entry1.file_name.localeCompare(entry2.file_name)
-    }
+  return entry1.file_name.localeCompare(entry2.file_name)
 }
