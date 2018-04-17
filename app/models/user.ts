5e2be295263afdd30f5ef4e73e1bcfeedabb63f6
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-17 20:26:45 +0100

    Added user schema

---
+++ app/models/user.ts
@@ -0,0 +1,43 @@
+import { Document, Schema, Model, model } from 'mongoose'
+import * as bcrypt from 'bcrypt-nodejs'
+
+// More hash rounds = more secure; 12 should be enough
+const hashRounds = 12
+
+/**
+ * Interface for user model.
+ */
+interface IUser extends Document {
+  email: string
+  password: string
+}
+
+/**
+ * User schema only has email address and password for now.
+ * Might be a good idea to add first name, last name, role, etc.
+ */
+const userSchema: Schema = new Schema({
+  local: {
+    email: String,
+    password: String,
+  }
+})
+
+/**
+ * Hashes passwords using bcrypt before storing them in the database.
+ */
+userSchema.methods.generateHash = (password: string): string => (
+  bcrypt.hashSync(password, bcrypt.genSaltSync(hashRounds), null)
+)
+
+/**
+ * Checks if the password is valid.
+ */
+userSchema.methods.validPassword = (password: string): string => (
+  bcrypt.compareSync(password, this.local.password)
+)
+
+/**
+ * Exposes model to the rest of the backend.
+ */
+export const User = model('User', userSchema)
